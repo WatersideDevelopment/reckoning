3dffea44fe283daaae17743789899e0acbe104c6
D. Rimron-Soutter <darran@xalior.com>
2015-01-24 19:44:38 +0000

    Adding in more routes...
    -Dx

---
+++ src/app/clients/clients.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('reckoning')
+    .controller('ClientsCtrl', function ($scope) {
+        $scope.data = {
+            selectedIndex: 1
+        };
+        $scope.next = function() {
+            $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2) ;
+        };
+        $scope.previous = function() {
+            $scope.data.selectedIndex = Math.max($scope.data.selectedIndex - 1, 0);
+        };
+        console.log($scope);
+    });
