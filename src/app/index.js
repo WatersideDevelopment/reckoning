2be8ae12a0438ff7f0546fcff0ced3b5996d65da
D. Rimron-Soutter <darran@xalior.com>
2015-01-24 20:48:01 +0000

    Code now looks reasonable, colour pallet not gross, and buttons work.
    No data. No fixed height forms... Needs more work :)
    
    -Dx

--- src/app/index.js
+++ src/app/index.js
@@ -22,16 +22,26 @@ angular.module('reckoning', ['ngAnimate', 'ngTouch', 'ngSanitize', 'ngRoute', 'n
     .config(function ($mdThemingProvider){
         console.log($mdThemingProvider);
         $mdThemingProvider.theme('default')
-            .primaryPalette('purple', {
-                'default': '400', // by default use shade 400 from the purple palette for primary intentions
-                'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
-                'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
-                'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+            .primaryPalette('grey', {
+                //'default': '400', // by default use shade 400 from the purple palette for primary intentions
+                //'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                //'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                //'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
             })
             // If you specify less than all of the keys, it will inherit from the
             // default shades
-            .accentPalette('pink', {
-                'default': '200' // use shade 200 for default, and keep all other shades the same
-            });
+            //.accentPalette('pink', {
+            //    'default': '200' // use shade 200 for default, and keep all other shades the same
+            //});
+        $mdThemingProvider.theme("default-dark", "default").dark();
+        $mdThemingProvider.theme('secondary')
+            .primaryPalette('indigo', {
+            })
+            // If you specify less than all of the keys, it will inherit from the
+            // default shades
+            //.accentPalette('blue', {
+            //    'default': '200' // use shade 200 for default, and keep all other shades the same
+            //});
+        $mdThemingProvider.theme("secondary-dark", "secondary").dark();
     })
 ;
